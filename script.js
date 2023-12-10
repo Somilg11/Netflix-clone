a54ade3f5d4bd9c52f55a634ff60330fbb0d2005
Somilg11 <gsomil93@gmail.com>
2023-12-10 16:56:35 +0530

    hh

---
+++ script.js
@@ -0,0 +1,25 @@
+var favFood = "grapes";
+var favThoughts = function(){
+    console.log("original fav food is "+ favFood);
+    var favFoodf = "sushi";
+    console.log("new fav foos is"+favFoodf );
+}
+favThoughts();
+
+
+
+
+
+function sayHi(){
+    let x = 10;
+    var y = 20;
+    const z = 30;
+    a = 40;
+    console.log(x);
+  console.log(y);
+  console.log(z);
+  console.log(a);  
+}
+sayHi()
+
+
